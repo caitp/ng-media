f690198eeea00a1b4da8ce321170f8d9594c3173
Caitlin Potter <caitpotter88@gmail.com>
2014-01-05 16:08:45 -0500

    Updates

---
+++ js/app.js
@@ -0,0 +1,300 @@
+var app = angular.module("media.example", ["ngAnimate", "ngRoute", "media"]);
+
+var tour = [
+  { url: '/', description: 'ng-media for AngularJS' },
+  { url: '/video', description: 'Directive: html5-video' },
+  { url: '/video/overlay', description: 'html5-video overlay' },
+  { url: '/fin', description: 'End of tour' }
+];
+
+app
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('', {
+        templateUrl: 'partials/intro.html'
+      })
+      .when('/', {
+        templateUrl: 'partials/intro.html'
+      })
+      .when('/video', {
+        templateUrl: 'partials/video.html',
+        controller: 'videoController'
+      })
+      .when('/video/overlay', {
+        templateUrl: 'partials/video.overlay.html',
+        controller: 'videoOverlayController'
+      })
+      .when('/fin', {
+        templateUrl: 'partials/fin.html'
+      })
+      .when('/404', {
+        templateUrl: 'partials/404.html',
+        controller: '404Controller'
+      })
+      .otherwise({ redirectTo: '/404' });
+  })
+  .run(function($rootScope) {
+    $rootScope.navoptions = tour;
+    $rootScope.$on('$routeChangeError', function($event) {
+      $location.path('/404');
+    });
+  });
+
+app.controller('404Controller', function($scope, $location, $timeout) {
+  $scope.time = 10;
+  $scope.home = '/';
+  $timeout(countdown, 1000);
+
+  function countdown() {
+    if ($scope.time-- === 0) {
+      $location.path('/');
+    } else {
+      $timeout(countdown, 1000);
+    }
+  }
+});
+
+app
+  .controller({
+    videoController: function($scope) {
+      $scope.mode = "example";
+    },
+    videoOverlayController: function($scope) {
+      $scope.mode = "example";
+    }
+  });
+
+app
+  .directive('addClass', function($timeout) {
+    return function(scope, element, attr) {
+      $timeout(function() {
+        element.addClass(attr.addClass);
+      }, 666); // this is pretty arbitrary, I know.
+    }
+  });
+
+app
+  .directive('navScroller', function($compile, $rootScope) {
+    return {
+      restrict: "A",
+      require: 'navScroller',
+      templateUrl: 'partials/nav-scroller.html',
+      replace: true,
+      scope: {
+        "routes": "=navScroller"
+      },
+      controller: function($scope, $location) {
+        var items = [], current, deltaY = 0, self = this, deltaThreshold = 1000, lastkey;
+
+        bind();
+
+        function unbind() {
+          angular.element(document)
+            .off('wheel', wheelnav)
+            .off('keydown keyup', keynav);
+        }
+
+        function bind() {
+          angular.element(document)
+            .bind('wheel', wheelnav)
+            .bind('keydown keyup', keynav);
+        }
+
+        function nav(next) {
+          deltaY = 0;
+          if (self.$selectRoute(next.$route())) {
+            $scope.$apply(function() {
+              $location.path(current.$route());
+            });
+            unbind();
+            setTimeout(bind, 500);
+          }
+        }
+
+        function keynav(event) {
+          if (event.type === 'keydown') {
+            lastkey = event.which;
+          } else if (event.type === 'keyup' && event.which === lastkey) {
+            var i = self.$index(current);
+            if (event.which === 38) {
+              nav(items[i-1]);
+            } else if (event.which === 40) {
+              nav(items[i+1]);
+            }
+          }
+        }
+
+        function wheelnav(event) {
+          deltaY += event.wheelDeltaY;
+          var next;
+          if (items.length) {
+            var i = self.$index(current);
+            if (deltaY <= -deltaThreshold) {
+              nav(next = items[i+1]);
+            } else if (deltaY >= deltaThreshold) {
+              nav(items[i-1]);
+            }
+          }
+        }
+
+        this.$index = function(navItem) {
+          for (var i = 0; i < items.length; ++i) {
+            if (navItem === items[i]) {
+              return i;
+            }
+          }
+          return -1;
+        }
+
+        this.$addItem = function(navItem) {
+          items.push(navItem);
+          if ($location.path() === navItem.$route()) {
+            this.$selectRoute(navItem.$route());
+          }
+        };
+        this.$selectRoute = function(route) {
+          if (current) {
+            if (route === current.$route()) {
+              return;
+            }
+            current = current.$deselect();
+          }
+          for (var i=0; i < items.length; ++i) {
+            var item = items[i];
+            if (item.$route() === route) {
+              current = item.$select();
+              return true;
+            }
+          }
+        };
+      },
+      link: function($scope, $element, $attr, self) {
+        angular.forEach($scope.routes, function(route) {
+          var description = route;
+          if (typeof route === 'object') {
+            description = route.description;
+            route = route.url;
+          }
+          var template = '<nav data-nav-scroller-item="' + route + '" data-nav-description="' + description + '"></nav>';
+          $compile(template)($scope, function(dom) {
+            $element.append(dom);
+          });
+        });
+
+        $rootScope.$on('$routeChangeSuccess', function($event, $route) {
+          self.$selectRoute($route.$$route.originalPath);
+        });
+      }
+    };   
+  })
+  .directive('navScrollerItem', function() {
+    return {
+      restrict: "A",
+      require: ['navScrollerItem', '^navScroller'],
+      templateUrl: 'partials/nav-scroller-item.html',
+      replace: true,
+      scope: true,
+      controller: function($scope, $element, $attrs) {
+        var route = $scope.route = $attrs.navScrollerItem;
+        $scope.description = $attrs.navDescription;
+        this.$route = function() {
+          return route;
+        };
+        this.$select = function() {
+          $attrs.$addClass('active');
+          return this;
+        };
+        this.$deselect = function() {
+          $attrs.$removeClass('active');
+        }
+      },
+      link: function($scope, $element, $attr, $controllers) {
+        $controllers[1].$addItem($controllers[0]);
+      }
+    };
+  });
+
+app
+  .directive('switcher', function($compile) {
+    return {
+      restrict: "A",
+      require: ['switcher', 'ngModel'],
+      templateUrl: 'partials/nav-switcher.html',
+      replace: true,
+      controller: function() {
+        var items = [], current;
+        this.$addItem = function(navItem) {
+          items.push(navItem);
+          if (navItem.$name() === this.$model.$viewValue) {
+            this.$select(navItem);
+          }
+        };
+        this.$find = function(item) {
+          for (var i = 0; i < items.length; ++i) {
+            var $it = items[i];
+            if ($it.$name() === item) {
+              return $it;
+            }
+          }
+        };
+        this.$select = function(item) {
+          if (typeof item === 'string') {
+            item = this.$find(item);
+          }
+          if (!item) {
+            return;
+          }
+          if (current) {
+            current = current.$deselect();
+          }
+          current = item.$select();
+          this.$model.$setViewValue(item.$name());
+        };
+      },
+      link: function($scope, $element, $attr, $controllers) {
+        var $self = $controllers[0], $model = $controllers[1];
+        $self.$model = $model;
+        var items = ($attr.switcher || '').split('|');
+        angular.forEach(items, function(item) {
+          var template = '<nav data-switcher-item="' + item + '"></nav>';
+          $compile(template)($scope, function(dom) {
+            $element.append(dom);
+          });
+        });
+        $model.$formatters.unshift(function(value) {
+          $self.$select(value);
+          return value;
+        });
+      }
+    };
+  })
+  .directive('switcherItem', function() {
+    return {
+      restrict: "A",
+      require: ['switcherItem', '^switcher'],
+      templateUrl: 'partials/nav-switcher-item.html',
+      replace: true,
+      scope: true,
+      controller: function($scope, $element, $attrs) {
+        var name = $scope.switcherItem = $attrs.switcherItem || '';
+        this.$name = function() {
+          return name;
+        };
+        this.$select = function() {
+          $attrs.$addClass('active');
+          return this;
+        };
+        this.$deselect = function() {
+          $attrs.$removeClass('active');
+        }
+      },
+      link: function($scope, $element, $attr, $controllers) {
+        $controllers[1].$addItem($controllers[0]);
+        $element.bind('click touch', function() {
+          $scope.$apply(function() {
+            $controllers[1].$select($controllers[0]);
+          });
+        });
+      }
+    };
+  });
---
+++ ng-media.js
@@ -0,0 +1,289 @@
+(function(window, document, undefined) {'use strict';
+
+var html5VideoController = [
+  '$scope',
+  '$injector',
+  '$attr',
+  'directiveName',
+  'element',
+  'video',
+  'overlay', function($scope, $injector, $attr, directiveName, element, video, overlay) {
+  var $parse = $injector.get('$parse'),
+      $interpolate = $injector.get('$interpolate'),
+      $http = $injector.get('$http'),
+      $templateCache = $injector.get('$templateCache'),
+      $compile = $injector.get('$compile');
+  
+  var exprs = {
+    track: $parse($attr.track || ''),
+    width: $parse($attr.width || ''),
+    height: $parse($attr.height || ''),
+    poster: $interpolate($attr.poster || '', false)
+  };
+
+  var _src,
+      _track,
+      _poster,
+      _width,
+      _height,
+      changeDetected = 0,
+      srcChanging = false,
+      srcChanged = false,
+      trackChanged = false,
+      _sources = [],
+      _tracks = [];
+
+  if (angular.isDefined($attr.controlsUrl) && $attr.controlsUrl.length) {
+    $attr.$set('controls', undefined);
+    var url;
+    try {
+      url = $scope.$eval($attr.controlsUrl);
+    } catch (e) {}
+
+    if (typeof url === 'undefined') {
+      url = $interpolate($attr.controlsUrl, false)($scope);
+    }
+
+    var videoCtrl = this;
+    $http.get(url, { cache: true }).then(function(res) {
+      var controlScope = $scope.$new();
+      controlScope.video = videoCtrl;
+      $compile(res.data)(controlScope, function(dom) {
+        angular.element(element).append(dom);
+      });
+    });
+  }
+
+  if (angular.isDefined($attr.controls) && ['false', '0'].indexOf($attr.controls) < 0) {
+    video.controls = true;
+  }
+
+  var $videoWatcher = function() {
+    var src,
+        track,
+        width = exprs.width($scope),
+        height = exprs.height($scope),
+        poster = exprs.poster($scope),
+        srcSource,
+        trackSource;
+
+    try {
+      srcSource = $attr.src || $attr[directiveName] || '';
+      src = $scope.$eval(srcSource);
+    } catch (e) {}
+    if (typeof src === 'undefined' && srcSource.length) {
+      src = $interpolate(srcSource, false)($scope);
+    }
+
+    try {
+      trackSource = $attr.track || '';
+      track = $scope.$eval(trackSource);
+    } catch (e) {}
+
+    if (typeof track === 'undefined' && trackSource.length) {
+      track = $interpolate(trackSource, false)($scope);
+    }
+
+    if (!angular.equals(src, _src)) {
+      ++changeDetected;
+      _src = src;
+      srcChanged = true;
+    }
+
+    if (!angular.equals(track, _track)) {
+      ++changeDetected;
+      _track = track;
+      trackChanged = true;
+    }
+
+    if (width !== _width) {
+      ++changeDetected;
+      _width = width;
+    }
+
+    if (height !== _height) {
+      ++changeDetected;
+      _height = height;
+    }
+
+    if (poster !== _poster) {
+      ++changeDetected;
+      _poster = poster;
+    }
+    return changeDetected;
+  };
+
+  var updateSrc = function(sources) {
+    angular.forEach(_sources, function(src, i) {
+      (src.parentNode || src.parentElement).removeChild(src);      
+    });
+    _sources = [];
+    var last = video.firstChild;
+    angular.forEach(sources, function(_src, i) {
+      var source = document.createElement('source'), src = _src;
+      if (typeof _src === 'object') {
+        source.type = _src.type;
+        source.media = _src.media;
+        src = _src.src;
+      }
+      if (!src) {
+        source = null;
+      } else {
+        source.src = src;
+        _sources.push(source);
+        video.insertBefore(source, last);
+        last = last && last.nextSibling;
+      }
+    });
+  };
+
+  var updateTrack = function(tracks) {
+    angular.forEach(_tracks, function(track, i) {
+      (track.parentNode || track.parentElement).removeChild(track);      
+    });
+    _tracks = [];
+    angular.forEach(tracks, function(_src, i) {
+      var track = document.createElement('track'), src = _src;
+      if (typeof _src === 'object') {
+        track.kind = _src.kind || 'subtitles';
+        track.charset = _src.charset;
+        track.srclang = _src.srclang;
+        track.label = _src.label;
+        
+        track.type = _src.type;
+        track.media = _src.media;
+        src = _src.src;
+      } else {
+        track.kind = 'subtitles';
+      }
+      if (!src) {
+        console.log("no src attribute!");
+        track = null;
+      } else {
+        track.src = src;
+        _tracks.push(track);
+        video.insertBefore(track, null);
+      }
+    });
+  };
+
+  var $videoUpdate = function() {
+    if (srcChanged) {
+      updateSrc(angular.isArray(_src) ? _src : [_src]);
+      srcChanging = true;
+      srcChanged = false;
+    }
+
+    if (trackChanged) {
+      updateTrack(angular.isArray(_track) ? _track : [_track]);
+      trackChanged = false;
+    }
+
+    video.width = _width;
+    video.height = _height;
+    video.poster = _poster;
+  };
+
+  $scope.$watch($videoWatcher, $videoUpdate);
+
+  this.$play = function() {
+    if (srcChanged) {
+      video.load();
+      srcChanged = false;
+    }
+    video.play();
+  };
+
+  this.$pause = function() {
+    video.pause();
+  };
+
+  this.$paused = function() {
+    return video.paused;
+  };
+
+  this.$playbackRate = function(rate) {
+    if (arguments.length) {
+      video.playbackRate = rate;
+      return this;
+    } else {
+      return video.playbackRate;
+    }
+  };
+
+  this.$volume = function(volume) {
+    if (arguments.length) {
+      video.volume = volume;
+      return this;
+    } else {
+      return video.volume;
+    }
+  };
+
+  this.$mute = function(setting) {
+    if (arguments.length) {
+      video.muted = !!setting;
+      return this;
+    } else {
+      return video.muted;
+    }
+  };
+}];
+
+var html5Video = ['$controller', '$interpolate', '$compile', '$http', '$templateCache',
+function($controller, $interpolate, $compile, $http, $templateCache) {
+  return {
+    restrict: "A",
+    transclude: true,
+    template: '<div class="html5-video"></div>',
+    replace: true,
+    compile: function(element, attr) {
+      var overlayUrl = attr.overlayUrl;
+      return function($scope, $element, $attr, undefined, $transclude) {
+        var video = document.createElement('video');
+        var attrOverlay;
+        var $overlay = angular.element('<div class="html5-video-overlay"></div>');
+        $element.append(video);
+        if (overlayUrl) {
+          $http.get($interpolate(overlayUrl, false)($scope), { cache: $templateCache })
+            .success(function(tpl) {
+              $overlay.html(tpl);
+              $compile($overlay)($scope, function(node) {
+                $element.append(node);
+              });
+            });
+          attrOverlay = true;
+        }
+
+        $element.data('$html5VideoController', $controller('html5VideoController', {
+          $scope: $scope,
+          $attr: $attr,
+          directiveName: 'html5Video',
+          element: $element[0],
+          video: video,
+          overlay: $overlay[0]
+        }));
+
+        if (!attrOverlay) {
+          $element.append($overlay);
+          $transclude($scope, function(dom) {
+            $overlay.append(dom);
+          });
+        }
+      };
+    }
+  };
+}];
+
+
+angular.module('media', [])
+.directive({
+  'html5Video': html5Video
+})
+.controller({
+  'html5VideoController': html5VideoController
+});
+
+!angular.$$csp() && angular.element(document).find('head').prepend('<style type="text/css">.html5-video{position:relative;overflow:auto;display:inline-block;margin:0;}.html5-video-overlay{margin:0;position:absolute;top:0;left:0;z-index:100;background:none;overflow:hidden;pointer-events:none;width:100%;height:100%;}</style>');
+
+})(window, document);
