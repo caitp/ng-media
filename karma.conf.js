1941c1d3ca5f2e8d60fc71cf66a0f590ac3fa50a
Caitlin Potter <caitpotter88@gmail.com>
2015-07-18 21:47:17 -0400

    chore(ci): upgrade browsers
    Firefox -> 37
    Safari -> 8.0

--- karma.conf.js
+++ karma.conf.js
@@ -55,13 +55,13 @@ module.exports = function(config) {
       'SL_Firefox': {
         base: 'SauceLabs',
         browserName: 'firefox',
-        version: '26'
+        version: '37'
       },
       'SL_Safari': {
         base: 'SauceLabs',
         browserName: 'safari',
-        platform: 'OS X 10.9',
-        version: '7'
+        platform: 'OS X 10.10',
+        version: '8'
       }
     },
   });
